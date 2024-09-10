36a304d17f44e06a381be206d273eafdf8a31d3b
Juliana Brondino <133527240+jubrondino@users.noreply.github.com>
2024-09-10 20:56:25 -0300

    Add files via upload
    Singleton em java de uma fila

---
+++ Fila.java
@@ -0,0 +1,21 @@
+public class Fila {
+    private static Fila filaimpressao;
+
+    private Fila() {
+
+    }
+
+    public static Fila filaimpressao() {
+        if (filaimpressao == null) {
+            filaimpressao = new filaimpressao();
+            return filaimpressao;
+        }
+
+        public void imprimeDocumento() {}
+        public void removeDocumento() {}
+        public void removeTodosDocumento() {}
+
+    }
+
+
+}
